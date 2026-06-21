471f45af580738986f1fdaa2961f6512efe431fe
ryyn08 <reyhanryyn@gmail.com>
2026-06-21 17:58:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+pkg update && pkg upgrade -y
+pkg install nodejs git curl -y
+npm install express cors
+chmod +x menu.sh
+echo -e "\e[1;32mINSTALASI SELESAI. JALANKAN DENGAN ./menu.sh\e[0m"
